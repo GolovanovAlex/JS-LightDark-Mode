afc1fa57477482cb8038fa81b7db6ba98440cd98
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-08-24 13:45:25 +0300

    js

--- app.js
+++ app.js
@@ -10,5 +10,10 @@ switchBtn.addEventListener('scroll', (e) => {
     body.style.background = `rgb(${y}, ${y}, ${y})`;
     btn.style.background = `rgb(${y}, ${y}, ${y})`;
     switchBtn.style.background = `rgb(${y - 12}, ${y - 8}, ${y - 6})`;
+  } else {
+    switchBtn.classList.add('active');
+    body.style.background = `rgb(${y + 30}, ${y + 34}, ${y + 38})`;
+    btn.style.background = `rgb(${y}, ${y + 8}, ${y + 16})`;
+    switchBtn.style.background = `rgb(${y + 20}, ${y + 24}, ${y + 28})`;
   }
 });
